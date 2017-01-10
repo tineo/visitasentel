bdbbd854e28f396cec7668c625173f515e14ef64
tineo <cesar@tineo.mobi>
2017-01-09 19:50:32 -0500

    + analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -136,5 +136,24 @@
         ga('send', 'pageview');
 
     </script>
+
+    <!-- Start of StatCounter Code for Dreamweaver -->
+    <script type="text/javascript">
+        var sc_project=11215030;
+        var sc_invisible=1;
+        var sc_security="4fd03f83";
+        var scJsHost = (("https:" == document.location.protocol) ?
+            "https://secure." : "http://www.");
+        document.write("<sc"+"ript type='text/javascript' src='" +
+            scJsHost+
+            "statcounter.com/counter/counter.js'></"+"script>");
+    </script>
+    <noscript><div class="statcounter">
+            <a title="web analytics" href="http://statcounter.com/" target="_blank">
+                <img class="statcounter"
+                     src="//c.statcounter.com/11215030/0/4fd03f83/1/" alt="web analytics">
+            </a></div>
+    </noscript>
+    <!-- End of StatCounter Code for Dreamweaver -->
 </body>
 </html>
