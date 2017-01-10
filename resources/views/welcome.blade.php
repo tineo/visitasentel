bf5e89e435943827f432d12ed29c57f681ba5412
tineo <cesar@tineo.mobi>
2017-01-09 19:06:44 -0500

    + analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 
         <title>{{ config('app.name', 'Visitas') }}</title>
 
-        <!-- Fonts
+
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
@@ -15,7 +15,7 @@
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                /*font-family: 'Raleway', sans-serif;*/
+                font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
