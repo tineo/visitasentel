0baeaaf2d1cf24473b3f0f14760033004055e693
tineo <cesar@tineo.mobi>
2017-01-16 02:06:50 -0500

    + .env

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,45 +30,72 @@ class HomeController extends Controller
     {
         $em = app('Doctrine\ORM\EntityManagerInterface');
         $roles = array();
-        $query1 = $em->createQuery("SELECT r FROM App\Entities\Role r WHERE r.name = :name");
-        $query1->setParameter("name", "admin");
-        $role = $query1->getOneOrNullResult();
-        if($role == null){
-            $r1 = new Role();
-            $r1->setName("admin");
-            $r1->setDescription("Administrador");
-            $em->persist($r1);
 
-            $roles[] = "admin";
+        $query0 = $em->createQuery("SELECT r FROM App\Entities\User r WHERE r.email = :email");
+        $query0->setParameter("name", "admin");
+        $user0 = $query0->getOneOrNullResult();
+        if($user0 == null) {
+
+            $query1 = $em->createQuery("SELECT r FROM App\Entities\Role r WHERE r.name = :name");
+            $query1->setParameter("name", "admin");
+            $role = $query1->getOneOrNullResult();
+            if ($role == null) {
+                $r1 = new Role();
+                $r1->setName("admin");
+                $r1->setDescription("Administrador");
+                $em->persist($r1);
+
+                $roles[] = "admin";
+            }
+
+            $query2 = $em->createQuery("SELECT r FROM App\Entities\Role r WHERE r.name = :name");
+            $query2->setParameter("name", "user");
+            $role2 = $query2->getOneOrNullResult();
+            if ($role2 == null) {
+                $r2 = new Role();
+                $r2->setName("user");
+                $r2->setDescription("Usuario");
+                $em->persist($r2);
+
+                $roles[] = "user";
+            }
+
+
+            $query3 = $em->createQuery("SELECT r FROM App\Entities\Role r WHERE r.name = :name");
+            $query3->setParameter("name", "clerk");
+            $role3 = $query3->getOneOrNullResult();
+            if ($role3 == null) {
+                $r3 = new Role();
+                $r3->setName("clerk");
+                $r3->setDescription("Recepcionista");
+                $em->persist($r3);
+
+                $roles[] = "clerk";
+            }
+
+            $em = app('Doctrine\ORM\EntityManagerInterface');
+            $user = new User("Cesar Gutierrez", "cesar@tineo.mobi", bcrypt("kokoro"));
+
+            $em->flush();
+
+            $query = $em->createQuery("SELECT r FROM App\Entities\Role r ");
+
+            $query->setParameter("id", $role);
+            $roles = $query->getResult();
+
+            foreach ($roles as $role) {
+                $user->getRoles()->add($role);
+            }
+            $em->persist($user);
+
+
+            $em->flush();
+
+
+            return view('home', array("roles" => $roles));
+        }else{
+            abort(404);
         }
-
-        $query2 = $em->createQuery("SELECT r FROM App\Entities\Role r WHERE r.name = :name");
-        $query2->setParameter("name", "user");
-        $role2 = $query2->getOneOrNullResult();
-        if($role2 == null){
-            $r2 = new Role();
-            $r2->setName("user");
-            $r2->setDescription("Usuario");
-            $em->persist($r2);
-
-            $roles[] = "user";
-        }
-
-
-        $query3 = $em->createQuery("SELECT r FROM App\Entities\Role r WHERE r.name = :name");
-        $query3->setParameter("name", "clerk");
-        $role3 = $query3->getOneOrNullResult();
-        if($role3 == null){
-            $r3 = new Role();
-            $r3->setName("clerk");
-            $r3->setDescription("Recepcionista");
-            $em->persist($r3);
-
-            $roles[] = "clerk";
-        }
-        $em->flush();
-
-        return view('home', array("roles" => $roles));
     }
 
     /**
